6607e0cde3e15bc04f475a692a9fe50378155ae7
king <lxt373@gmail.com>
2025-08-11 14:30:09 +0800

    登录时的表单数据验证规则

--- router/user.js
+++ router/user.js
@@ -19,6 +19,6 @@ const router = express.Router();
 router.post("/reguser", expressJoi(reg_login_schema), userHandler.regUser);
 
 // 登录
-router.post("/login", userHandler.login);
+router.post("/login", expressJoi(reg_login_schema), userHandler.login);
 
 module.exports = router;
