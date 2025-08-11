38a589f70e76fa6fdfa94c17a595e480c93048c5
king <lxt373@gmail.com>
2025-08-11 16:00:09 +0800

    feat: 登录成功后，根据Id，查询用户信息

--- router_handler/userInfo.js
+++ router_handler/userInfo.js
@@ -1,3 +1,14 @@
+const db = require("../db/index"); // 导入数据库操作模块
+
+// 获取用户信息
 exports.getUserInfo = (req, res) => {
-  res.send({ status: 0, message: "获取用户信息成功", data: req.user });
+  // 根据id查询用户信息
+  const sql = `select id, username, nickname, email, user_pic from ev_users where id=?`;
+  db.query(sql, [req.user.id], (err, result) => {
+    if (err) return res.cc(err);
+    if (result.length !== 1) {
+      return res.cc("获取用户信息失败");
+    }
+    res.send({ status: 0, message: "获取用户信息成功", data: result[0] });
+  });
 };
